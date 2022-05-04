81109787d4350c02fd9e0df03ea9ceaa73524d98
yurimonti <48514670+yurimonti@users.noreply.github.com>
2022-05-04 09:44:05 +0200

    add files

---
+++ Spring_Security_Example/src/test/java/com/example/Spring_Security_Example/SpringSecurityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Security_Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
